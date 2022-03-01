3d6a6d4672599825234b8cfac2d754d7642ff0b6
leen-1999 <80642456+leen-1999@users.noreply.github.com>
2022-03-01 14:55:37 +0300

    L005dealingWithData

---
+++ L005dealingWithData.py
@@ -0,0 +1,10 @@
+# Our Apps Contains Code + Data
+# Code is The Lines You Write To Manage and Deal With this Data
+# To Structure The Data We Need To Categorize [Num, String, Boolean]
+# Num =>Students Age, Students phone Number, Teachers Salaries
+# String => students Name, Teachers Name, Events Name
+# Booleans => Teacher Got Salary Or No, Students Age Is Suitable or No
+# Data is Stored on Computer Memory
+# We Use Variable To refer To This Data
+# Variables Not Containing The Data Its Only Refer To ıts Location on Memory
+# Code is Using The Data Too Perform Operation[Add, Edit, Delete]
