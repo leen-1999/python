0ae38371c062e927fb892952996f99f42c75795f
leen-1999 <80642456+leen-1999@users.noreply.github.com>
2022-03-01 14:13:05 +0300

    L004comments

---
+++ L004comments.py
@@ -0,0 +1,22 @@
+# ---------------------------------
+# Information About File
+# License
+# Who Created This File
+# When The File Created
+# Write Beside The programming Line
+# Write Before The Programming Line
+# Prevent Code From Run
+# ---------------------------------
+
+# This is comment
+print("I love programming")  # THis is Inline comment
+#  comment/uncomment klavye kısaltması ctrl+ö (türkçe klavye)
+#  comment/uncomment klavye kısaltması ctrl+ ظ(arapça klavye)
+
+"""
+This is 
+Not   
+Multiple
+Line Comments
+"""
+# The lines above are just a string isn't assigned
